90403001fac74628acb6d94559b113656cfe2953
Rhea-Anne Danao <158535192+rheaanne@users.noreply.github.com>
2024-12-16 16:31:26 +0800

    added java demo

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -1,3 +1,9 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
   }
+
+function myFunctio2() {
+  const element = document.getElementsByTagName("p");
+
+  document.getElementById("demo").innerHTML = '<br>The text in first paragraph (index 0) is: </br>' + element[0].innerHTML;
+}
