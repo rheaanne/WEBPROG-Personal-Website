8edecb68379da8822073101aaed910354bebe22d
Rhea-Anne Danao <158535192+rheaanne@users.noreply.github.com>
2025-01-15 00:00:51 +0800

    update

--- home/js/image.js
+++ home/js/image.js
@@ -1,12 +1,13 @@
 const Gallery = Vue.createApp({
-    data() {
-     return {
-       Gallery: [
-         {name: 'Family', url: 'https://i.ibb.co/G5V5nxM/image2.jpg'},
-         {name: 'Myself', url: 'https://i.ibb.co/G5V5nxM/image2.jpg'},
-       ]
-     }
-    }
-   })
-   Gallery.mount('#Gallery')
+  data() {
+      return {
+          Gallery: [
+              { name: 'Family', url: 'https://i.ibb.co/G5V5nxM/image2.jpg' },
+              { name: 'Myself', url: 'https://i.ibb.co/G5V5nxM/image2.jpg' },
+          ],
+      };
+  },
+});
+
+Gallery.mount('#Gallery');
 
