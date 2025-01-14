425c8f6ca7552f944c52667d89e6731e1b89cf93
Rhea-Anne Danao <158535192+rheaanne@users.noreply.github.com>
2025-01-14 23:57:04 +0800

    update

--- home/js/image.js
+++ home/js/image.js
@@ -2,8 +2,8 @@ const Gallery = Vue.createApp({
     data() {
      return {
        Gallery: [
-         {name: 'Burrito', url: 'https://i.ibb.co/G5V5nxM/image2.jpg'},
-         {name: 'Salad', url: 'https://i.ibb.co/G5V5nxM/image2.jpg'},
+         {name: 'Family', url: 'https://i.ibb.co/G5V5nxM/image2.jpg'},
+         {name: 'Myself', url: 'https://i.ibb.co/G5V5nxM/image2.jpg'},
        ]
      }
     }
